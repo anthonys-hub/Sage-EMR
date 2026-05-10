dfa4209a6c2e7ba94c4ddfd4a89f9c48d51e22f4
Anthony <anthonyblur13@gmail.com>
2026-05-10 19:30:12 -0400

    patients done

--- server/routes/patients.js
+++ server/routes/patients.js
@@ -23,6 +23,33 @@ router.get('/:id', authMiddleware, async (req, res) => {
 
 )
 
+router.post('/', authMiddleware, async (req, res) => {
+    try {
+        const { first_name, last_name, email, dob, address, mobile_phone, home_phone, marriage_status, title, social_security, emergency_contact } = req.body
+        const newPatient = await pool.query('INSERT INTO patients (first_name, last_name, email, dob, address, mobile_phone, home_phone, marriage_status, title, social_security, emergency_contact) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11) RETURNING patient_id', [first_name, last_name, email, dob, address, mobile_phone, home_phone, marriage_status, title, social_security, emergency_contact])
+        return res.status(201).json({ message: 'Patient added!', patient_id: newPatient.rows[0].patient_id })
+    }
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: 'Cannot add patient' })
+    }
+})
+
+router.put('/:id', authMiddleware, async (req, res) => {
+    try {
+        const { first_name, last_name, email, dob, address, mobile_phone, home_phone, marriage_status, title, social_security, emergency_contact } = req.body
+        const patient_id = req.params.id
+        const edit = await pool.query('UPDATE patients SET first_name = $1, last_name = $2, email = $3, dob = $4, address = $5, mobile_phone = $6, home_phone = $7, marriage_status = $8, title = $9, social_security = $10, emergency_contact = $11 WHERE patient_id = $12', [first_name, last_name, email, dob, address, mobile_phone, home_phone, marriage_status, title, social_security, emergency_contact, patient_id])
+        return res.status(200).json({ message: 'Patient changes saved' });
+    }
+
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
 
 
 
