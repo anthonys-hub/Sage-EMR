cbd9d68db4185f95c4e30aa66bbf7cf5b3ce4444
Anthony <anthonyblur13@gmail.com>
2026-05-10 20:48:58 -0400

    cases made

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ const users = require('./routes/users.js')
 const patients = require('./routes/patients.js')
 const appointments = require('./routes/appointments.js')
 const doctors = require('./routes/doctors.js')
-
+const cases = require('./routes/cases.js')
 
 
 dotenv.config()
@@ -23,6 +23,7 @@ app.use('/api/users', users)
 app.use('/api/patients', patients)
 app.use('/api/appointments', appointments)
 app.use('/api/doctors', doctors)
+app.use('/api/cases', cases)
 
 
 
---
+++ server/routes/cases.js
@@ -0,0 +1,52 @@
+const express = require('express')
+const router = express.Router()
+const pool = require('../db/db.js')
+const requireAdmin = require('../middleware/requireAdmin.js')
+const authMiddleware = require('../middleware/authMiddleware.js')
+
+
+router.get('/:id', authMiddleware, async (req, res) => {
+
+    try {
+        const result = await pool.query('SELECT case_id, description, referring_dr, patient_id FROM cases WHERE case_id = $1', [req.params.id])
+        const cases = result.rows[0]
+        return res.status(200).json(cases)
+    }
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: 'Case not found!' })
+    }
+}
+)
+
+router.post('/', authMiddleware, async (req, res) => {
+    try {
+        const { description, referring_dr, patient_id } = req.body
+        const newCase = await pool.query('INSERT INTO cases ( description, referring_dr, patient_id ) VALUES ($1, $2, $3) RETURNING case_id', [description, referring_dr, patient_id])
+        return res.status(201).json({ message: 'Case added!', case_id: newCase.rows[0].case_id })
+    }
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: 'Cannot add case!' })
+    }
+})
+
+router.put('/:id', authMiddleware, async (req, res) => {
+    try {
+        const { description, referring_dr, patient_id } = req.body
+        const CaseId = req.params.id
+        const edit = await pool.query('UPDATE cases SET  description = $1,  referring_dr = $2, patient_id = $3 WHERE case_id = $4', [description, referring_dr, patient_id, CaseId])
+        return res.status(200).json({ message: 'Case changes saved' });
+    }
+
+
+    catch (err) {
+        console.log(err)
+        return res.status(500).json({ message: 'Server error' });
+    }
+})
+
+
+module.exports = router
